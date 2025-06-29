7dd60dfd12a9a97ced3d5c789498400fc5f70f26
Xiaoyi Qu <xiaoyiqu@umich.edu>
2025-06-29 04:22:42 +0000

    add code

--- hw1.py
+++ hw1.py
@@ -5,11 +5,13 @@ This assignment involves building a question answering model using the Google T5
 import json
 import nltk
 import torch
+import matplotlib.pyplot as plt
 from torch.optim import AdamW
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 from torch.utils.data import Dataset, DataLoader
 from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
 nltk.download('punkt')
+nltk.download('punkt_tab')
 
 # context, answer, question, and ID format
 # Read json dataset line by line
@@ -21,7 +23,7 @@ def load_qa_data(json_path):
     return data
 
 
-def generate_answer(question, context, tokenizer, model, device="cpu", max_len=64):
+def generate_answer(question, context, tokenizer, model, device="cuda", max_len=64):
     input_text = f"question: {question} context: {context}"
     inputs = tokenizer(input_text, return_tensors="pt", truncation=True, padding=True).to(device)
     with torch.no_grad():
@@ -86,7 +88,9 @@ class QADataset(Dataset):
 # Load tokenizer and model
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 tokenizer = T5Tokenizer.from_pretrained("t5-base")
-model = T5ForConditionalGeneration.from_pretrained("t5-base").to(device) # fix the error here
+model = T5ForConditionalGeneration.from_pretrained("t5-base") # fix the error here
+
+model = model.to(device)
 
 train_data = load_qa_data("data/train.json")
 test_data = load_qa_data("data/dev.json")
@@ -100,11 +104,12 @@ test_loader = DataLoader(test_dataset, batch_size=8)
 # Train the model
 optimizer = AdamW(model.parameters(), lr=5e-5)
 num_epochs = 3
+train_losses = []
 
 model.train()
 for epoch in range(num_epochs):
     total_loss = 0
-    for batch in train_loader:
+    for i, batch in enumerate(train_loader):
         input_ids = batch['input_ids'].to(device)
         attention_mask = batch['attention_mask'].to(device)
         labels = batch['labels'].to(device)
@@ -118,20 +123,42 @@ for epoch in range(num_epochs):
         total_loss += loss.item()
         # print(f"Loss: {total_loss / len(train_loader):.4f}")
 
+        # avg_loss = total_loss / len(train_loader)
+        train_losses.append(loss.detach().cpu())
+
+        # if i == 10:
+        #     break
     print(f"Epoch {epoch+1}/{num_epochs} | Loss: {total_loss / len(train_loader):.4f}")
 
+'''
+loss function curve
+'''
+plt.figure(figsize=(8, 6))
+plt.plot(range(1, len(train_losses) + 1), train_losses, marker='o', label='Training Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.title('Training Loss Convergence Curve')
+plt.grid(True)
+plt.legend()
+plt.tight_layout()
+plt.savefig("loss_curve.pdf")
 
-# model evaluation
+
+'''
+model evaluation
+'''
 model.eval()
 test_data = load_qa_data("data/dev.json")  # 每个样本含 question, context, answer
 
 predictions = []
 references = []
 
-for item in test_data:
+for i,item in enumerate(test_data):
     pred = generate_answer(item["question"], item["context"], tokenizer, model)
     predictions.append(pred)
     references.append(item["answer"])
+    # if i == 10:
+    #     break
 
 bleu_scores = evaluate_bleu(predictions, references)
 
@@ -139,4 +166,7 @@ print("BLEU Scores:")
 for k, v in bleu_scores.items():
     print(f"{k}: {v:.4f}")
 
+# Epoch 1/3 | Loss: 0.1215
+# Epoch 2/3 | Loss: 0.0330
+# Epoch 3/3 | Loss: 0.0286
 
